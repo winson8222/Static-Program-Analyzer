99bdd0c25cc1aac774402e0d7ad6bf0ab869b092
GWphua <phuaguanwei99@gmail.com>
2024-02-04 00:13:03 +0800

    Implement Lexical Tokens methods

--- Team19/Code19/src/spa/src/sp/LexicalToken.cpp
+++ Team19/Code19/src/spa/src/sp/LexicalToken.cpp
@@ -1,25 +1,21 @@
+// Include the header file for the LexicalToken class.
 #include "LexicalToken.h"
-#include <iostream>
+#include <regex>
 
 namespace LexicalTokens {
-    bool Letter::verifyToken(std::string str) const {
-        std::cout << "Letter class verifyToken" << std::endl;
-        return true;
-    }
+	bool Letter::verifyToken(std::string str) const {
+		return std::regex_match(str, std::regex(R"([A-Za-z])"));
+	}
 
-    bool Digit::verifyToken(std::string str) const {
-        std::cout << "Digit class verifyToken" << std::endl;
-        return true;
-    }
+	bool Digit::verifyToken(std::string str) const {
+		return std::regex_match(str, std::regex(R"(\d)"));
+	}
 
-    bool Name::verifyToken(std::string str) const {
-        std::cout << "Name class verifyToken" << std::endl;
-        return true;
-    }
+	bool Name::verifyToken(std::string str) const {
+		return std::regex_match(str, std::regex(R"([A-Za-z][A-Za-z0-9]*)"));
+	}
 
-    bool Integer::verifyToken(std::string str) const {
-        std::cout << "Integer class verifyToken" << std::endl;
-        return true;
-    }
-
+	bool Integer::verifyToken(std::string str) const {
+		return std::regex_match(str, std::regex(R"(\d+)"));
+	}
 }
--- Team19/Code19/src/spa/src/sp/LexicalToken.h
+++ Team19/Code19/src/spa/src/sp/LexicalToken.h
@@ -3,29 +3,29 @@
 
 /* Contains classes describing SIMPLE Lexical Tokens. */
 namespace LexicalTokens {
-    class LexicalToken {
-    public:
-        virtual bool verifyToken(std::string str) const; 
-        virtual ~LexicalToken() = default;
-    };
+	class LexicalToken {
+	public:
+		virtual bool verifyToken(std::string str) const = 0;
+		virtual ~LexicalToken() = default;
+	};
 
-    class Letter : public LexicalToken {
-    public:
-        bool verifyToken(std::string str) const override;
-    };
+	class Letter : public LexicalToken {
+	public:
+		bool verifyToken(std::string str) const override;
+	};
 
-    class Digit : public LexicalToken {
-    public:
-        bool verifyToken(std::string str) const override;
-    };
+	class Digit : public LexicalToken {
+	public:
+		bool verifyToken(std::string str) const override;
+	};
 
-    class Name : public LexicalToken {
-    public:
-        bool verifyToken(std::string str) const override;
-    };
+	class Name : public LexicalToken {
+	public:
+		bool verifyToken(std::string str) const override;
+	};
 
-    class Integer : public LexicalToken {
-    public:
-        bool verifyToken(std::string str) const override;
-    };
-} 
+	class Integer : public LexicalToken {
+	public:
+		bool verifyToken(std::string str) const override;
+	};
+}
