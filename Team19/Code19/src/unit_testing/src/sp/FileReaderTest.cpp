8935b914163811d542f8ba3656685266bbd5ab6e
hmanhduc2k <hmanhduc2k@gmail.com>
2024-02-05 00:03:04 +0800

    Fix bugs on test cases of tokenizer

--- Team19/Code19/src/unit_testing/src/sp/FileReaderTest.cpp
+++ Team19/Code19/src/unit_testing/src/sp/FileReaderTest.cpp
@@ -1,25 +1,20 @@
 #include "sp/Tokenizer.h"
 #include "catch.hpp"
 #include <fstream>
+#include <filesystem>
 
 TEST_CASE("Tokenizer::readFileToString", "[readFileToString]") {
     const std::string testFileName = "../../../../../tests/sourcefile1.txt";
-    const std::string testFileContent = "This is a test file content.";
-    // Initialize the tempFile variable
-    std::ofstream tempFile;
-
-    // Open the test file and check if it's open
-    tempFile.open(testFileName);
-    REQUIRE(tempFile.is_open());
-    tempFile << testFileContent;
-    tempFile.close();
+    const std::string testFileContent = "x = 1;";
+    REQUIRE(std::filesystem::exists(testFileName));
 
     std::string actualContent;
     REQUIRE_NOTHROW(actualContent = Tokenizer::readFileToString(testFileName));
 
+    // Print the actual content and expected content for debugging
+    INFO("Actual content:\n" << actualContent);
+    INFO("Expected content:\n" << testFileContent);
+
     // Assert that the actual content matches the expected content
     REQUIRE(actualContent == testFileContent);
-
-    // Remove the temporary test file
-    std::remove(testFileName.c_str());
 }
