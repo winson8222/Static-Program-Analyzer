5fb33803eab03c6b70930bf822ce48fe93f331b3
jianoway <77208762+jianoway@users.noreply.github.com>
2024-02-08 15:32:55 +0800

    feat: Add some definitions to PKBReader and PKBWriter

--- Team19/Code19/src/spa/src/pkb/PKBReader.h
+++ Team19/Code19/src/spa/src/pkb/PKBReader.h
@@ -9,31 +9,92 @@ private:
     shared_ptr<PKB> pkb;
 
     shared_ptr<FollowsStore> followsStore;
+    shared_ptr<FollowsTStore> followsTStore;
+
+    shared_ptr<ParentStore> parentStore;
+    shared_ptr<ParentTStore> parentTStore;
+
+    shared_ptr<UsesPStore> usesPStore;
+    shared_ptr<UsesSStore> usesSStore;
+
+    shared_ptr<ModifiesPStore> modifiesPStore;
+    shared_ptr<ModifiesSStore> modifiesSStore;
 
     shared_ptr<AssignStore> assignStore;
     shared_ptr<VariableStore> variableStore;
+    shared_ptr<ConstantStore> constantStore;
+    shared_ptr<ProcedureStore> procedureStore;
+    shared_ptr<StatementStore> statementStore;
+    shared_ptr<IfStore> ifStore;
+    shared_ptr<WhileStore> whileStore;
+    shared_ptr<CallStore> callStore;
+    shared_ptr<ReadStore> readStore;
+    shared_ptr<PrintStore> printStore;
 
 public:
     explicit PKBReader(const shared_ptr<PKB>& pkb): pkb(pkb) {
         followsStore = pkb->getFollowsStore();
+        followsTStore = pkb->getFollowsTStore();
+
+        parentStore = pkb->getParentStore();
+        parentTStore = pkb->getParentTStore();
+
+        usesPStore = pkb->getUsesPStore();
+        usesSStore = pkb->getUsesSStore();
+
+        modifiesPStore = pkb->getModifiesPStore();
+        modifiesSStore = pkb->getModifiesSStore();
+
         assignStore = pkb->getAssignStore();
         variableStore = pkb->getVariableStore();
+        constantStore = pkb->getConstantStore();
+        procedureStore = pkb->getProcedureStore();
+        statementStore = pkb->getStatementStore();
+        ifStore = pkb->getIfStore();
+        whileStore = pkb->getWhileStore();
+        callStore = pkb->getCallStore();
+        readStore = pkb->getReadStore();
+        printStore = pkb->getPrintStore();
     }
     // Entities
+
+    /**
+     * @brief Gets the statement number of all "Assign" entities.
+     * @return unordered_set<int>: Statement number of all "Assign" entities.
+     */
     unordered_set<int> getAllAssigns() {
         return assignStore->getAllEntities();
     }
 
+    /**
+     * @brief Gets the statement number of all "Variable" entities.
+     * @return unordered_set<int>: Statement number of all "Variable" entities.
+     */
     unordered_set<string> getAllVariables() {
         return variableStore->getAllEntities();
     }
 
+    // Relationships
+
+    /**
+     * @brief Gets the statement number of reversed "Follows" relationships of the given statement.
+     * @param stmt: stmt in Follows(s1, stmt)
+     * @return unordered_set<int>: Statement number of all s1 in Follows(s1, stmt)
+     */
     unordered_set<int> getPreFollows(int stmt) {
         return followsStore->getRelationshipsByValue(stmt);
     }
+
+    /**
+     * @brief Gets the statement number of "Follows" relationships of the given statement.
+     * @param stmt: stmt in Follows(stmt, s2)
+     * @return unordered_set<int>: Statement number of all s2 in Follows(stmt, s2)
+     */
     unordered_set<int> getPostFollows(int stmt) {
         return followsStore->getRelationshipsByKey(stmt);
     }
+
+
 };
 
 
--- Team19/Code19/src/spa/src/pkb/PKBWriter.h
+++ Team19/Code19/src/spa/src/pkb/PKBWriter.h
@@ -9,61 +9,124 @@ private:
     shared_ptr<PKB> pkb;
 
     shared_ptr<FollowsStore> followsStore;
+    shared_ptr<FollowsTStore> followsTStore;
+
+    shared_ptr<ParentStore> parentStore;
+    shared_ptr<ParentTStore> parentTStore;
+
+    shared_ptr<UsesPStore> usesPStore;
+    shared_ptr<UsesSStore> usesSStore;
+
+    shared_ptr<ModifiesPStore> modifiesPStore;
+    shared_ptr<ModifiesSStore> modifiesSStore;
 
     shared_ptr<AssignStore> assignStore;
     shared_ptr<VariableStore> variableStore;
-
+    shared_ptr<ConstantStore> constantStore;
+    shared_ptr<ProcedureStore> procedureStore;
+    shared_ptr<StatementStore> statementStore;
+    shared_ptr<IfStore> ifStore;
+    shared_ptr<WhileStore> whileStore;
+    shared_ptr<CallStore> callStore;
+    shared_ptr<ReadStore> readStore;
+    shared_ptr<PrintStore> printStore;
 
 public:
     explicit PKBWriter(const shared_ptr<PKB>& pkb): pkb(pkb) {
-        followsStore = pkb->getFollowsStore();
+      followsStore = pkb->getFollowsStore();
+      followsTStore = pkb->getFollowsTStore();
 
-        assignStore = pkb->getAssignStore();
-        variableStore = pkb->getVariableStore();
+      parentStore = pkb->getParentStore();
+      parentTStore = pkb->getParentTStore();
+
+      usesPStore = pkb->getUsesPStore();
+      usesSStore = pkb->getUsesSStore();
+
+      modifiesPStore = pkb->getModifiesPStore();
+      modifiesSStore = pkb->getModifiesSStore();
+
+      assignStore = pkb->getAssignStore();
+      variableStore = pkb->getVariableStore();
+      constantStore = pkb->getConstantStore();
+      procedureStore = pkb->getProcedureStore();
+      statementStore = pkb->getStatementStore();
+      ifStore = pkb->getIfStore();
+      whileStore = pkb->getWhileStore();
+      callStore = pkb->getCallStore();
+      readStore = pkb->getReadStore();
+      printStore = pkb->getPrintStore();
     }
     // Entities
-    void insertConstant(int constant);
-    void insertVariable(string variable) {
-        variableStore->addEntity(variable);
+
+    /**
+     * @brief Inserts a "Constant" entity into the PKB.
+     * @param constant
+     */
+    void insertConstant(int &constant) {
+      constantStore->addEntity(constant);
     }
-    void insertProcedure(string procedure);
-    void insertStatement(int stmtNum);
-    void insertIf(int stmtNum);
-    void insertWhile(int stmtNum);
-    void insertCall(int stmtNum);
-    void insertAssign(int stmtNum) {
-        assignStore->addEntity(stmtNum);
+
+    /**
+     * @brief Inserts a "Variable" Entity into the PKB.
+     * @param variable
+     */
+    void insertVariable(string &variable) {
+      variableStore->addEntity(variable);
     }
-    void insertRead(int stmtNum) {
-        throw "Not implemented";
+    void insertProcedure(string &procedure) {
+      procedureStore->addEntity(procedure);
     }
-    void insertPrint(int stmtNum) {
-        throw "Not implemented";
+
+    void insertStatement(int &stmtNum) {
+      statementStore->addEntity(stmtNum);
+    }
+    void insertIf(int &stmtNum) {
+      ifStore->addEntity(stmtNum);
+    }
+    void insertWhile(int &stmtNum) {
+      whileStore->addEntity(stmtNum);
+    }
+    void insertCall(int &stmtNum) {
+      callStore->addEntity(stmtNum);
+    }
+    void insertAssign(int &stmtNum) {
+      assignStore->addEntity(stmtNum);
+    }
+    void insertRead(int &stmtNum) {
+      readStore->addEntity(stmtNum);
+    }
+    void insertPrint(int &stmtNum) {
+      printStore->addEntity(stmtNum);
     }
 
     // Relationships
-    void insertFollows(int stmt1, int stmt2) {
-        followsStore->addRelationship(stmt1, stmt2);
+    /**
+     * @brief Inserts a Follows relationship "Follows(stmt1, stmt2)".
+     * @param stmt1
+     * @param stmt2
+     */
+    void insertFollows(int &stmt1, int &stmt2) {
+      followsStore->addRelationship(stmt1, stmt2);
     }
-    void insertFollowsT(int stmt1, int stmt2) {
+    void insertFollowsT(int &stmt1, int &stmt2) {
+      followsTStore->addRelationship(stmt1, stmt2);
+    }
+    void insertParent(int &stmt1, int &stmt2) {
+       parentStore->addRelationship(stmt1, stmt2);
+    }
+    void insertParentT(int &stmt1, int &stmt2) {
+      parentTStore->addRelationship(stmt1, stmt2);
+    }
+    void insertUsesP(string &procedure, string &variable) {
+      usesPStore->addRelationship(procedure, variable);
+    }
+    void insertUsesS(int &stmt, string &variable) {
         throw "Not implemented";
     }
-    void insertParent(int stmt1, int stmt2) {
+    void insertModifiesP(string &procedure, string &variable) {
         throw "Not implemented";
     }
-    void insertParentT(int stmt1, int stmt2) {
-        throw "Not implemented";
-    }
-    void insertUsesP(int stmt, string variable) {
-        throw "Not implemented";
-    }
-    void insertUsesS(int stmt, string variable) {
-        throw "Not implemented";
-    }
-    void insertModifiesP(int stmt, string variable) {
-        throw "Not implemented";
-    }
-    void insertModifiesS(int stmt, string variable) {
+    void insertModifiesS(int &stmt, string &variable) {
         throw "Not implemented";
     }
 
