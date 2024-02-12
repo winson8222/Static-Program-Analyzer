9b2044dc488a2491b869a1d7f8b49687802f9474
Guan Wei <phuaguanwei99@gmail.com>
2024-02-12 19:29:25 +0800

    Add ReadStmt classes

---
+++ Team19/Code19/src/spa/src/sp/Parser/ReadStmt.cpp
@@ -0,0 +1,30 @@
+#include "ReadStmt.h"
+#include "sp/LexicalToken.h"
+#include <iostream>
+
+ReadStmt::ReadStmt(const LexicalToken& variable, int start, int end) {
+    this->variable = variable;
+    this->lines = std::make_pair(start, end); 
+}
+
+int ReadStmt::getStartLine() const {
+    return this->lines.first;
+}
+
+int ReadStmt::getEndLine() const {
+    return this->lines.second;
+}
+
+std::shared_ptr<ASTNode> ReadStmt::buildTree() {
+    std::shared_ptr<ASTNode> tree = std::make_shared<ASTNode>(
+        ASTNodeType::READ, this->lines.first, Utility::getASTNodeType(ASTNodeType::READ)
+    );
+
+    std::shared_ptr<ASTNode> child = std::make_shared<ASTNode>(
+        ASTNodeType::VARIABLE, this->lines.second, this->variable.getValue()
+    );
+
+    tree.get()->addChild(child);
+
+    return tree;
+}
