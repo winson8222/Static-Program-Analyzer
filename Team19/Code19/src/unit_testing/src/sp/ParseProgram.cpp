2e0156f04aa805228b931b87fc1e01f39b6ddf49
Guan Wei <phuaguanwei99@gmail.com>
2024-02-13 01:46:45 +0800

    Add parseProgram Unit Tests

--- Team19/Code19/src/unit_testing/src/sp/ParseProgram.cpp
+++ Team19/Code19/src/unit_testing/src/sp/ParseProgram.cpp
@@ -2,8 +2,8 @@
 #include "sp/Parser/SimpleParser.h"
 #include <filesystem>
 
-TEST_CASE("Procedure parsing throws an error for missing closing curly brace", "[parseProcedure]") {
-	const std::string testFileName = "../../../../../tests/sp/ParserTest/Procedure_Invalid1.txt";
+TEST_CASE("Program parsing throws an error for missing curly brace", "[parseProgram]") {
+	const std::string testFileName = "../../../../../tests/sp/ParserTest/Program_Invalid1.txt";
 	REQUIRE(std::filesystem::exists(testFileName));
 	SimpleParser parser(testFileName);
 
@@ -12,7 +12,7 @@ TEST_CASE("Procedure parsing throws an error for missing closing curly brace", "
 }
 
 
-TEST_CASE("Procedure parsing throws an error for missing curly braces", "[parseProcedure]") {
+TEST_CASE("Program parsing throws an error for missing curly braces", "[parseProgram]") {
 	const std::string testFileName = "../../../../../tests/sp/ParserTest/Procedure_Invalid2.txt";
 	REQUIRE(std::filesystem::exists(testFileName));
 	SimpleParser parser(testFileName);
@@ -21,20 +21,26 @@ TEST_CASE("Procedure parsing throws an error for missing curly braces", "[parseP
 	CHECK_THROWS_AS(parser.parseProcedure(), std::runtime_error);
 }
 
-TEST_CASE("Calling parseProcedure with print, read, call statements", "[parseProcedure]") {
+TEST_CASE("Calling parseProgram with print, read, call statements", "[parseProgram]") {
 	// Generate test file
 	const std::string testFileName = "../../../../../tests/sp/ParserTest/Procedure1.txt";
 	REQUIRE(std::filesystem::exists(testFileName));
 	SimpleParser parser(testFileName);
-	std::shared_ptr<ASTNode> tree_ptr = parser.parseProcedure();
+	std::shared_ptr<ASTNode> tree_ptr = parser.parseProgram();
 
-	REQUIRE(tree_ptr->type == ASTNodeType::PROCEDURE);
+	REQUIRE(tree_ptr->type == ASTNodeType::PROGRAMS);
 	REQUIRE(tree_ptr->lineNumber == 1);
-	REQUIRE(tree_ptr->value == Utility::getASTNodeType(ASTNodeType::PROCEDURE));
+	REQUIRE(tree_ptr->value == Utility::getASTNodeType(ASTNodeType::PROGRAMS));
+
+
+	const auto procedure = (tree_ptr->children)[0];
+	REQUIRE(procedure->type == ASTNodeType::PROCEDURE);
+	REQUIRE(procedure->lineNumber == 1);
+	REQUIRE(procedure->value == Utility::getASTNodeType(ASTNodeType::PROCEDURE));
+
 
 	SECTION("Testing child nodes") {
-		const auto& statementList = (tree_ptr->children)[0];
-
+		const auto& statementList = (procedure->children)[0];
 
 		REQUIRE(statementList->type == ASTNodeType::STATEMENT_LIST);
 		REQUIRE(statementList->lineNumber == 1);
@@ -44,20 +50,39 @@ TEST_CASE("Calling parseProcedure with print, read, call statements", "[parsePro
 			const auto& statements = statementList->children;
 			REQUIRE(statements.size() == 3);
 
-			auto& callStatement = statements[0];
+			auto callStatement = statements[0];
 			REQUIRE(callStatement->type == ASTNodeType::CALL);
 			REQUIRE(callStatement->lineNumber == 2);
 			REQUIRE(callStatement->value == Utility::getASTNodeType(ASTNodeType::CALL));
 
-			auto& printStatement = statements[1];
+			auto printStatement = statements[1];
 			REQUIRE(printStatement->type == ASTNodeType::PRINT);
 			REQUIRE(printStatement->lineNumber == 3);
 			REQUIRE(printStatement->value == Utility::getASTNodeType(ASTNodeType::PRINT));
 
-			auto& readStatement = statements[2];
+			auto readStatement = statements[2];
 			REQUIRE(readStatement->type == ASTNodeType::READ);
 			REQUIRE(readStatement->lineNumber == 4);
 			REQUIRE(readStatement->value == Utility::getASTNodeType(ASTNodeType::READ));
 		}
 	}
+}
+
+TEST_CASE("Calling parseProgram for multiple procedures", "[parseProgram]") {
+	// Generate test file
+	const std::string testFileName = "../../../../../tests/sp/ParserTest/Program1.txt";
+	REQUIRE(std::filesystem::exists(testFileName));
+	SimpleParser parser(testFileName);
+	std::shared_ptr<ASTNode> tree_ptr = parser.parseProgram();
+
+	REQUIRE(tree_ptr->type == ASTNodeType::PROGRAMS);
+	REQUIRE(tree_ptr->lineNumber == 1);
+	REQUIRE(tree_ptr->value == Utility::getASTNodeType(ASTNodeType::PROGRAMS));
+
+
+	const auto& procedures = tree_ptr->children;
+	REQUIRE(procedures.size() == 3);
+
+	REQUIRE(procedures[1]->type == ASTNodeType::PROCEDURE);
+	REQUIRE(procedures[1]->value == Utility::getASTNodeType(ASTNodeType::PROCEDURE));
 }
