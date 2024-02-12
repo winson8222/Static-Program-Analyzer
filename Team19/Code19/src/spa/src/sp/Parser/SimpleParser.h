de8138eaabb98a6494efb5f8f1cfbdecb878c43e
Guan Wei <phuaguanwei99@gmail.com>
2024-02-12 21:03:21 +0800

    Fix Extra Qualification Error in MacOS CI

--- Team19/Code19/src/spa/src/sp/Parser/SimpleParser.h
+++ Team19/Code19/src/spa/src/sp/Parser/SimpleParser.h
@@ -11,32 +11,32 @@
 // Prompt: https://platform.openai.com/playground/p/cJLjmmneCEs4z6ms7ZkBSxJB?model=gpt-4&mode=chat
 class SimpleParser {
 public:
-    SimpleParser(std::string);
-    void parseProgram();
+	SimpleParser(std::string);
+	void parseProgram();
 
-// private:
-    std::vector<LexicalToken> tokenStream;
-    int tokenIndex;
-    bool hasTokensLeft() const;
-    LexicalToken peekToken() const;
-    LexicalToken SimpleParser::getToken();
-    void parseProcedure();
-    void parseStmtLst();
-    void parseStmt();
-    ReadStmt parseRead();
-    PrintStmt parsePrint();
-    CallStmt parseCall();
-    void parseWhile();
-    void parseIf();
-    void parseAssign();
-    void parseCondExpr();
-    void parseRelExpr();
-    void parseRelFactor();
-    void parseExpr();
-    void parseTerm();
-    void parseFactor();
-    void parseVarName();
-    void parseProcName();
-    void parseConstValue();
+	// private:
+	std::vector<LexicalToken> tokenStream;
+	int tokenIndex;
+	bool hasTokensLeft() const;
+	LexicalToken peekToken() const;
+	LexicalToken getToken();
+	void parseProcedure();
+	void parseStmtLst();
+	void parseStmt();
+	ReadStmt parseRead();
+	PrintStmt parsePrint();
+	CallStmt parseCall();
+	void parseWhile();
+	void parseIf();
+	void parseAssign();
+	void parseCondExpr();
+	void parseRelExpr();
+	void parseRelFactor();
+	void parseExpr();
+	void parseTerm();
+	void parseFactor();
+	void parseVarName();
+	void parseProcName();
+	void parseConstValue();
 };
 // ai-gen end
