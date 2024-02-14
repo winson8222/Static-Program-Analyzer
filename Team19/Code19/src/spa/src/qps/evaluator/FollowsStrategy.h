ebc02f3fbc8c4dd51cb6a415683a889bf1e08ca9
MSI\vegit <vegita2009@hotmail.com>
2024-02-15 01:33:49 +0300

    Refactor and add different cases to follow strategy

--- Team19/Code19/src/spa/src/qps/evaluator/FollowsStrategy.h
+++ Team19/Code19/src/spa/src/qps/evaluator/FollowsStrategy.h
@@ -9,5 +9,20 @@ using namespace std;
 class FollowsStrategy : public QueryEvaluationStrategy {
 public:
     unordered_set<string> evaluateQuery(PKBReaderManager& pkbReaderManager, const ParsingResult& parsingResult) override;
+    void fillResult(const unordered_set<int>& follows, unordered_set<string>& result);
+    void FollowsStrategy::processFirstParam(const Token& secondParam, const string& variant,
+        shared_ptr<FollowsReader> followsReader,
+        shared_ptr<FollowsTReader> followsTReader,
+        unordered_set<string>& result);
+    void FollowsStrategy::processSecondParam(const Token& firstParam, const string& variant,
+        shared_ptr<FollowsReader> followsReader,
+        shared_ptr<FollowsTReader> followsTReader,
+        unordered_set<string>& result);
+    bool FollowsStrategy::isBothParamsWildcard(const Token& firstParam, const Token& secondParam);
+    bool FollowsStrategy::isBothParamsInteger(const Token& firstParam, const Token& secondParam);
+    void FollowsStrategy::processIntegerParams(const Token& firstParam, const Token& secondParam,
+        shared_ptr<FollowsReader> followsReader,
+        shared_ptr<StatementReader> statementReader,
+        unordered_set<string>& result);
 };
 
