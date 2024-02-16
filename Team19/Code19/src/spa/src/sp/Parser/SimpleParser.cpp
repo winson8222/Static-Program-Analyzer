9a113badcfc7bea25b3eae21c5b5700506db29fd
Guan Wei <phuaguanwei99@gmail.com>
2024-02-16 13:50:37 +0800

    Remove unnecessary whitespace checks

--- Team19/Code19/src/spa/src/sp/Parser/SimpleParser.cpp
+++ Team19/Code19/src/spa/src/sp/Parser/SimpleParser.cpp
@@ -5,9 +5,7 @@
 // ai-gen start(gpt,2,e)
 // Prompt: https://platform.openai.com/playground/p/cJLjmmneCEs4z6ms7ZkBSxJB?model=gpt-4&mode=chat
 
-SimpleParser::SimpleParser() {
-
-}
+SimpleParser::SimpleParser() {}
 
 SimpleParser::SimpleParser(std::string filename) {
 	this->tokenStream = SPTokenizer::tokenize(SPTokenizer::readFileToString(filename));
@@ -18,11 +16,6 @@ std::shared_ptr<ASTNode> SimpleParser::parseProgram() {
 	std::vector<std::shared_ptr<ASTNode>> procedures;
 
 	while (this->hasTokensLeft()) {
-		if (this->peekToken().getTokenType() == LexicalTokenType::WHITESPACE) {
-			this->getToken();
-			continue;
-		}
-
 		procedures.push_back(this->parseProcedure());
 	}
 
@@ -104,10 +97,6 @@ std::shared_ptr<ASTNode> SimpleParser::parseStmtLst() {
 	std::vector<std::shared_ptr<ASTNode>> statements;
 	int firstLine = this->peekToken().getLine();
 	while (this->peekToken().getTokenType() != LexicalTokenType::SYMBOL_CLOSE_BRACE) {
-		if (this->peekToken().getTokenType() == LexicalTokenType::WHITESPACE) {
-			this->getToken();
-			continue;
-		}
 		statements.push_back(this->parseStmt());
 	}
 
