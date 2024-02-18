427a68b31e498b37fbe82fedc534d599a83711b4
hmanhduc2k <hmanhduc2k@gmail.com>
2024-02-19 05:05:47 +0800

    Add extractor classes for DesignExtractor

--- Team19/Code19/src/spa/src/sp/DesignExtractor/FacadeDesignExtractor.h
+++ Team19/Code19/src/spa/src/sp/DesignExtractor/FacadeDesignExtractor.h
@@ -5,13 +5,20 @@
 #include "sp/AST/ASTNode.h" // Assuming you have ASTNode defined in ASTNode.h
 #include "sp/DesignExtractor/Visitor/IVisitor.h"
 
+#include "sp/DesignExtractor/Visitor/AssignVisitor.h"
+#include "sp/DesignExtractor/Visitor/CallVisitor.h"
+#include "sp/DesignExtractor/Visitor/IfElseThenVisitor.h"
+#include "sp/DesignExtractor/Visitor/PrintVisitor.h"
+#include "sp/DesignExtractor/Visitor/ReadVisitor.h"
+#include "sp/DesignExtractor/Visitor/WhileVisitor.h"
+#include "sp/DesignExtractor/Visitor/ProcedureVisitor.h"
+
 class FDesignExtractor {
 public:
     FDesignExtractor(std::shared_ptr<ASTNode> root, std::shared_ptr<PKBWriterManager> pkbWriterManager)
         : root(root), pkbWriterManager(pkbWriterManager) {}
 
-    void extractAll() {
-    }
+    void extractAll();
 
 private:
     std::shared_ptr<ASTNode> root;
