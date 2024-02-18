973d33ba463e33cacd708b4d360e537046c2026f
GWphua <phuaguanwei99@gmail.com>
2024-02-18 20:48:29 +0800

    Rewrie parsePrint according to refactors

--- Team19/Code19/src/unit_testing/src/sp/ParserTest/ParsePrintStatement_AI.cpp
+++ Team19/Code19/src/unit_testing/src/sp/ParserTest/ParsePrintStatement_AI.cpp
@@ -1,39 +1,29 @@
-/*
 #include "catch.hpp"
-#include "sp/Parser/SimpleParser.h"
+#include "sp/Parser/SimpleParserFacade.h"
 #include <filesystem>
-// ai-gen start(gpt,2,e)
-// Prompt: https://platform.openai.com/playground/p/BL0U9QBHRxSO8JyQGo0KpEUZ?mode=chat
-TEST_CASE("PrintStmt parsing throws an error with invalid syntax", "[SimpleParser]") {
-	const std::string testFileName = "../../../../../tests/sp/ParserTest/CallStmt1.txt";
-	REQUIRE(std::filesystem::exists(testFileName));
-	SimpleParser parser(testFileName);
 
-	// For this test, we expect parsing to terminate due to erroneous input.
-	CHECK_THROWS_AS(parser.parsePrint(), std::runtime_error);
-}
-// ai-gen end
 
 // ai-gen start(gpt, 2, e)
 // Prompt: https://platform.openai.com/playground/p/CRO1bXNAQZB3Adua8DmfPuWb?model=gpt-4&mode=chat
-TEST_CASE("Testing PrintStmt::buildTree()", "[PrintStmt]") {
+TEST_CASE("Testing PrintStmt::buildTree()", "[parse][print]") {
 	// Generate test file
 	const std::string testFileName = "../../../../../tests/sp/ParserTest/PrintStmt1.txt";
 	REQUIRE(std::filesystem::exists(testFileName));
-	SimpleParser parser(testFileName);
-	std::shared_ptr<ASTNode> tree_ptr = parser.parsePrint();
+	SimpleParserFacade parser(testFileName);
+	std::shared_ptr<ASTNode> tree_ptr = parser.parse();
+	std::shared_ptr<ASTNode> printStatement = (((tree_ptr->children)[0]->children)[0]->children)[0];
 
-	REQUIRE(tree_ptr->type == ASTNodeType::PRINT);
-	REQUIRE(tree_ptr->lineNumber == 1);
-	REQUIRE(tree_ptr->value == Utility::getASTNodeType(ASTNodeType::PRINT));
+
+	REQUIRE(printStatement->type == ASTNodeType::PRINT);
+	REQUIRE(printStatement->lineNumber == 2);
+	REQUIRE(printStatement->value == Utility::getASTNodeType(ASTNodeType::PRINT));
 
 	SECTION("Testing tree child node") {
-		const auto& children = tree_ptr->children;
+		const auto& children = printStatement->children;
 		REQUIRE(children.size() == 1);
 		REQUIRE(children[0]->type == ASTNodeType::VARIABLE);
-		REQUIRE(children[0]->lineNumber == 1);
-		REQUIRE(children[0]->value == "x");
+		REQUIRE(children[0]->lineNumber == 2);
+		REQUIRE(children[0]->value == "read");
 	}
 }
 // ai-gen end
-*/
