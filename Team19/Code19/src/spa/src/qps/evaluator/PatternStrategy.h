b37fbdb3de024aa51d8a956a32313ad0cd0b5e9c
winson8222 <winson8222@hotmail.com>
2024-02-19 19:44:56 +0800

    commented out Pattern for test

--- Team19/Code19/src/spa/src/qps/evaluator/PatternStrategy.h
+++ Team19/Code19/src/spa/src/qps/evaluator/PatternStrategy.h
@@ -1,23 +1,23 @@
-#pragma
-#define PATTERNSTRATEGY_H
-
-#include "qps/evaluator/suchThatStrategies/StmtStmtStrategy.h"
-
-
-using namespace std;
-
-class PatternStrategy : public StmtStmtStrategy {
-private:
-    std::shared_ptr<PatternReader> patternReader;
-    std::shared_ptr<AssignReader> assignReader;
-
-
-public:
-    unordered_set<string> evaluateQuery(PKBReaderManager& pkbReaderManager, const ParsingResult& parsingResult) override;
-
-    void processLHS(const Token& firstParam, unordered_set<string>& result) override;
-    void processRHS(const Token& secondParam, unordered_set<string>& result, bool partialMatch) override;
-    void combineResults(const unordered_set<string>& newResult, unordered_set<string>& result);
-
-};
-
+//#pragma
+//#define PATTERNSTRATEGY_H
+//
+//#include "qps/evaluator/suchThatStrategies/StmtStmtStrategy.h"
+//
+//
+//using namespace std;
+//
+//class PatternStrategy {
+//private:
+//    std::shared_ptr<PatternReader> patternReader;
+//    std::shared_ptr<AssignReader> assignReader;
+//
+//
+//public:
+//    unordered_set<string> evaluateQuery(PKBReaderManager& pkbReaderManager, const ParsingResult& parsingResult);
+//
+//    void processLHS(const Token& firstParam, unordered_set<string>& result);
+//    void processRHS(const Token& secondParam, unordered_set<string>& result, bool partialMatch);
+//    void combineResults(const unordered_set<int>& newResult, unordered_set<string>& result);
+//    static void fillResult(const unordered_set<int>& follows, unordered_set<string>& result);
+//};
+//
--- Team19/Code19/src/spa/src/qps/evaluator/suchThatStrategies/ModifiesSStrategy.cpp
+++ Team19/Code19/src/spa/src/qps/evaluator/suchThatStrategies/ModifiesSStrategy.cpp
@@ -1,12 +1,12 @@
+////
+//// Created by Winson Zheng on 18/2/24.
+////
 //
-// Created by Winson Zheng on 18/2/24.
+//#include "ModifiesSStrategy.h"
 //
-
-#include "ModifiesSStrategy.h"
-
-void ModifiesSStrategy::processFirstParam(const Token& secondParam, const string& variant,
-                                         unordered_set<string>& result) {
-    if (storage->isModifiesS(stoi(secondParam.getValue()))) {
-        result = storage->getModifiedVar(stoi(secondParam.getValue()));
-    }
-}
+//void ModifiesSStrategy::processFirstParam(const Token& secondParam, const string& variant,
+//                                         unordered_set<string>& result) {
+//    if (storage->isModifiesS(stoi(secondParam.getValue()))) {
+//        result = storage->getModifiedVar(stoi(secondParam.getValue()));
+//    }
+//}
