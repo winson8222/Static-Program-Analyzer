fcee56f061fb1257f49412de13917c61417a7217
winson8222 <winson8222@hotmail.com>
2024-02-22 23:55:10 +0800

    Change some test cases

---
+++ Team19/Code19/src/spa/src/qps/evaluator/suchThatStrategies/EntEntStrategy.h
@@ -0,0 +1,19 @@
+#include "SuchThatStrategy.h"
+using namespace std;
+
+class EntEntStrategy : public SuchThatStrategy {
+
+public:
+    ~EntEntStrategy() override = default;
+
+    virtual void processBothSynonyms(const Token& firstParam, const Token& secondParam, const ParsingResult& parsingResult
+            ,std::shared_ptr<ResultTable> resultTable, PKBReaderManager& pkbReaderManager) = 0;
+    virtual void processFirstParam(const Token& firstParam, const Token& secondParam, const ParsingResult& parsingResult
+            ,std::shared_ptr<ResultTable> resultTable, PKBReaderManager& pkbReaderManager) = 0;
+    virtual void processSecondParam(const Token& firstParam, const Token& secondParam, const ParsingResult& parsingResult
+            ,std::shared_ptr<ResultTable> resultTable, PKBReaderManager& pkbReaderManager) = 0;
+    virtual void processBothConstants(const Token& firstParam, const Token& secondParam, const ParsingResult& parsingResult
+            ,std::shared_ptr<ResultTable> resultTable) = 0;
+    virtual void processWildCards(const Token& firstParam, const Token& secondParam,std::shared_ptr<ResultTable> resultTable) = 0;
+
+};
