c5bdbc3d511f19d9bbd0e0626a197a339d099ca6
Virushade <70288012+GWphua@users.noreply.github.com>
2024-02-25 18:04:09 +0800

    Update script to create logs folder if it does not exist.

--- Team19/Tests19/system_testing/m1/window_test.ps1
+++ Team19/Tests19/system_testing/m1/window_test.ps1
@@ -1,6 +1,21 @@
 Write-Host "[System Test] Running all tests for Milestone 1"
 $EXIT_CODE = 0
 
+# Get the current directory
+$currentDirectory = Get-Location
+
+# Combine current directory path with 'logs' directory
+$logsDirectory = Join-Path -Path $currentDirectory -ChildPath "logs"
+
+# Check if 'logs' directory exists
+if (-not (Test-Path -Path $logsDirectory -PathType Container)) {
+    # 'logs' directory doesn't exist, create it
+    New-Item -Path $logsDirectory -ItemType Directory
+    Write-Host "Directory 'logs' created in $currentDirectory."
+} else {
+    Write-Host "Directory 'logs' already exists in $currentDirectory."
+}
+
 foreach ($file in Get-ChildItem source/*_source.txt) {
     Write-Host "Run test"
     $test_name = ($file.Name -split "_source.txt")[0]  # Extract test name from file name
