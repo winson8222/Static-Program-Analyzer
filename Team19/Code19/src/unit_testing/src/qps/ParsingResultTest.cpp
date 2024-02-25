60e342d454b848af678746baad0e903f1ae023b9
MSI\vegit <vegita2009@hotmail.com>
2024-02-25 15:37:48 +0300

    Comment out unneeded test cases for M1

--- Team19/Code19/src/unit_testing/src/qps/ParsingResultTest.cpp
+++ Team19/Code19/src/unit_testing/src/qps/ParsingResultTest.cpp
@@ -204,70 +204,70 @@ TEST_CASE("Check Parsing Result of valid tokens that Follows with variable and w
 
 
 
-TEST_CASE("Check Parsing Result of Pattern with variable and an expressionSpec") {
-    std::vector<Token> tokens = {
-            Token(TokenType::DesignEntity, "assign"),
-            Token(TokenType::IDENT, "a"),
-            Token(TokenType::Semicolon, ";"),
-            Token(TokenType::SelectKeyword, "Select"),
-            Token(TokenType::IDENT, "a"),
-            Token(TokenType::PatternKeyword, "pattern"),
-            Token(TokenType::IDENT, "a"),
-            Token(TokenType::Lparenthesis, "("),
-            Token(TokenType::QuoutIDENT, "\"x\""),
-            Token(TokenType::Comma, ","),
-            Token(TokenType::DoubleQuote, "\""),
-            Token(TokenType::INTEGER, "1"),
-            Token(TokenType::Operator, "*"),
-            Token(TokenType::INTEGER, "3"),
-            Token(TokenType::DoubleQuote, "\""),
-            Token(TokenType::Rparenthesis, ")")
-    };
+//TEST_CASE("Check Parsing Result of Pattern with variable and an expressionSpec") {
+//    std::vector<Token> tokens = {
+//            Token(TokenType::DesignEntity, "assign"),
+//            Token(TokenType::IDENT, "a"),
+//            Token(TokenType::Semicolon, ";"),
+//            Token(TokenType::SelectKeyword, "Select"),
+//            Token(TokenType::IDENT, "a"),
+//            Token(TokenType::PatternKeyword, "pattern"),
+//            Token(TokenType::IDENT, "a"),
+//            Token(TokenType::Lparenthesis, "("),
+//            Token(TokenType::QuoutIDENT, "\"x\""),
+//            Token(TokenType::Comma, ","),
+//            Token(TokenType::DoubleQuote, "\""),
+//            Token(TokenType::INTEGER, "1"),
+//            Token(TokenType::Operator, "*"),
+//            Token(TokenType::INTEGER, "3"),
+//            Token(TokenType::DoubleQuote, "\""),
+//            Token(TokenType::Rparenthesis, ")")
+//    };
+//
+//    QueryParser parser(tokens);
+//    auto parsingResult = parser.parse();
+//    REQUIRE(parsingResult.getDeclaredSynonym("a") == "assign");
+//    REQUIRE(parsingResult.getRequiredSynonym() == "a");
+//    REQUIRE(parsingResult.getPatternClauseRelationship().getType() == TokenType::IDENT);
+//    REQUIRE(parsingResult.getPatternClauseRelationship().getValue() == "a");
+//    REQUIRE(parsingResult.getPatternClauseFirstParam().getType() == TokenType::QuoutIDENT);
+//    REQUIRE(parsingResult.getPatternClauseFirstParam().getValue() == "\"x\"");
+//    REQUIRE(parsingResult.getPatternClauseSecondParam().getType() == TokenType::ExpressionSpec);
+//    REQUIRE(parsingResult.getPatternClauseSecondParam().getValue() == "\"1*3\"");
+//}
 
-    QueryParser parser(tokens);
-    auto parsingResult = parser.parse();
-    REQUIRE(parsingResult.getDeclaredSynonym("a") == "assign");
-    REQUIRE(parsingResult.getRequiredSynonym() == "a");
-    REQUIRE(parsingResult.getPatternClauseRelationship().getType() == TokenType::IDENT);
-    REQUIRE(parsingResult.getPatternClauseRelationship().getValue() == "a");
-    REQUIRE(parsingResult.getPatternClauseFirstParam().getType() == TokenType::QuoutIDENT);
-    REQUIRE(parsingResult.getPatternClauseFirstParam().getValue() == "\"x\"");
-    REQUIRE(parsingResult.getPatternClauseSecondParam().getType() == TokenType::ExpressionSpec);
-    REQUIRE(parsingResult.getPatternClauseSecondParam().getValue() == "\"1*3\"");
-}
 
-
-TEST_CASE("Check Parsing Result of Pattern with variable and an expressionSpec that contains an expression of _\"expr\"_ format") {
-    std::vector<Token> tokens = {
-            Token(TokenType::DesignEntity, "assign"),
-            Token(TokenType::IDENT, "a"),
-            Token(TokenType::Semicolon, ";"),
-            Token(TokenType::SelectKeyword, "Select"),
-            Token(TokenType::IDENT, "a"),
-            Token(TokenType::PatternKeyword, "pattern"),
-            Token(TokenType::IDENT, "a"),
-            Token(TokenType::Lparenthesis, "("),
-            Token(TokenType::QuoutIDENT, "\"x\""),
-            Token(TokenType::Comma, ","),
-            Token(TokenType::Wildcard, "_"),
-            Token(TokenType::DoubleQuote, "\""),
-            Token(TokenType::INTEGER, "1"),
-            Token(TokenType::Operator, "*"),
-            Token(TokenType::INTEGER, "3"),
-            Token(TokenType::DoubleQuote, "\""),
-            Token(TokenType::Wildcard, "_"),
-            Token(TokenType::Rparenthesis, ")")
-    };
-
-    QueryParser parser(tokens);
-    auto parsingResult = parser.parse();
-    REQUIRE(parsingResult.getDeclaredSynonym("a") == "assign");
-    REQUIRE(parsingResult.getRequiredSynonym() == "a");
-    REQUIRE(parsingResult.getPatternClauseRelationship().getType() == TokenType::IDENT);
-    REQUIRE(parsingResult.getPatternClauseRelationship().getValue() == "a");
-    REQUIRE(parsingResult.getPatternClauseFirstParam().getType() == TokenType::QuoutIDENT);
-    REQUIRE(parsingResult.getPatternClauseFirstParam().getValue() == "\"x\"");
-    REQUIRE(parsingResult.getPatternClauseSecondParam().getType() == TokenType::ExpressionSpec);
-    REQUIRE(parsingResult.getPatternClauseSecondParam().getValue() == "_\"1*3\"_");
-
-}
+//TEST_CASE("Check Parsing Result of Pattern with variable and an expressionSpec that contains an expression of _\"expr\"_ format") {
+//    std::vector<Token> tokens = {
+//            Token(TokenType::DesignEntity, "assign"),
+//            Token(TokenType::IDENT, "a"),
+//            Token(TokenType::Semicolon, ";"),
+//            Token(TokenType::SelectKeyword, "Select"),
+//            Token(TokenType::IDENT, "a"),
+//            Token(TokenType::PatternKeyword, "pattern"),
+//            Token(TokenType::IDENT, "a"),
+//            Token(TokenType::Lparenthesis, "("),
+//            Token(TokenType::QuoutIDENT, "\"x\""),
+//            Token(TokenType::Comma, ","),
+//            Token(TokenType::Wildcard, "_"),
+//            Token(TokenType::DoubleQuote, "\""),
+//            Token(TokenType::INTEGER, "1"),
+//            Token(TokenType::Operator, "*"),
+//            Token(TokenType::INTEGER, "3"),
+//            Token(TokenType::DoubleQuote, "\""),
+//            Token(TokenType::Wildcard, "_"),
+//            Token(TokenType::Rparenthesis, ")")
+//    };
+//
+//    QueryParser parser(tokens);
+//    auto parsingResult = parser.parse();
+//    REQUIRE(parsingResult.getDeclaredSynonym("a") == "assign");
+//    REQUIRE(parsingResult.getRequiredSynonym() == "a");
+//    REQUIRE(parsingResult.getPatternClauseRelationship().getType() == TokenType::IDENT);
+//    REQUIRE(parsingResult.getPatternClauseRelationship().getValue() == "a");
+//    REQUIRE(parsingResult.getPatternClauseFirstParam().getType() == TokenType::QuoutIDENT);
+//    REQUIRE(parsingResult.getPatternClauseFirstParam().getValue() == "\"x\"");
+//    REQUIRE(parsingResult.getPatternClauseSecondParam().getType() == TokenType::ExpressionSpec);
+//    REQUIRE(parsingResult.getPatternClauseSecondParam().getValue() == "_\"1*3\"_");
+//
+//}
