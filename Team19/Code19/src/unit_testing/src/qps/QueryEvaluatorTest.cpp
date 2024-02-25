ab62c9ae131e784ba831929686619904d9bbf264
MSI\vegit <vegita2009@hotmail.com>
2024-02-25 18:50:01 +0300

    Add test case for 2 clauses in evaluator

--- Team19/Code19/src/unit_testing/src/qps/QueryEvaluatorTest.cpp
+++ Team19/Code19/src/unit_testing/src/qps/QueryEvaluatorTest.cpp
@@ -919,3 +919,68 @@ TEST_CASE("Check Evaluation result of synonym for Parent with same stmt being us
     std::unordered_set<string> res = evaluator.evaluateQuery();
     REQUIRE(res == std::unordered_set<string>{});
 }
+
+//stmt s;
+//Select s such that Follows (s, s)
+TEST_CASE("Check Evaluation result of pattern and follows") {
+    std::shared_ptr<PKBManager> pkbManager = std::make_shared<PKBManager>();
+    std::shared_ptr<PKBReaderManager> pkbReaderManager = pkbManager->getPKBReaderManager();
+    std::shared_ptr<PKBWriterManager> pkbWriterManager = pkbManager->getPKBWriterManager();
+
+
+    std::shared_ptr<StatementWriter> statementWriter = pkbWriterManager->getStatementWriter();
+    std::shared_ptr<FollowsWriter> followsWriter = pkbWriterManager->getFollowsWriter();
+    std::shared_ptr<AssignPatternWriter> assignPatternWriter = pkbWriterManager->getAssignPatternWriter();
+    std::shared_ptr<AssignWriter> assignWriter = pkbWriterManager->getAssignWriter();
+
+    statementWriter->insertStatement(1);
+    statementWriter->insertStatement(2);
+    statementWriter->insertStatement(3);
+    statementWriter->insertStatement(4);
+
+    followsWriter->addFollows(1, 2);
+
+    assignPatternWriter->addAssignPattern(1, "x", "1");
+    assignPatternWriter->addAssignPattern(2, "x", "2");
+    assignPatternWriter->addAssignPattern(3, "x", "3");
+
+    assignWriter->insertAssign(1);
+    assignWriter->insertAssign(2);
+    assignWriter->insertAssign(3);
+
+
+
+    std::vector<Token> tokens = {
+            Token(TokenType::DesignEntity, "stmt"),
+            Token(TokenType::IDENT, "s"),
+            Token(TokenType::Semicolon, ";"),
+            Token(TokenType::DesignEntity, "assign"),
+            Token(TokenType::IDENT, "a"),
+            Token(TokenType::Semicolon, ";"),
+            Token(TokenType::SelectKeyword, "Select"),
+            Token(TokenType::IDENT, "s"),
+            Token(TokenType::PatternKeyword, "pattern"),
+            Token(TokenType::IDENT, "a"),
+            Token(TokenType::Lparenthesis, "("),
+            Token(TokenType::QuoutIDENT, "\"x\""),
+            Token(TokenType::Comma, ","),
+            Token(TokenType::QuoutConst, "\"1\""),
+            Token(TokenType::Rparenthesis, ")"),
+            Token(TokenType::SuchKeyword, "such"),
+            Token(TokenType::ThatKeyword, "that"),
+            Token(TokenType::Follows, "Follows"),
+            Token(TokenType::Lparenthesis, "("),
+            Token(TokenType::INTEGER, "1"),
+            Token(TokenType::Comma, ","),
+            Token(TokenType::INTEGER, "2"),
+            Token(TokenType::Rparenthesis, ")"),
+            
+
+    };
+
+    QueryParser parser(tokens);
+    auto parsingResult = parser.parse();
+    QueryEvaluator evaluator(pkbReaderManager, parsingResult);
+    std::unordered_set<string> res = evaluator.evaluateQuery();
+    REQUIRE(res == std::unordered_set<string>{"1", "2", "3", "4"});
+}
