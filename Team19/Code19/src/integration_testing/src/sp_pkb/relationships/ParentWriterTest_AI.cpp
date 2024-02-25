528bc456ee8dba9b8d86f11ce894b980ab00327b
hmanhduc2k <hmanhduc2k@gmail.com>
2024-02-25 15:13:18 +0800

    Fix test case issues

--- Team19/Code19/src/integration_testing/src/sp_pkb/relationships/ParentWriterTest_AI.cpp
+++ Team19/Code19/src/integration_testing/src/sp_pkb/relationships/ParentWriterTest_AI.cpp
@@ -9,8 +9,6 @@
 #include "sp/SourceProcessor.h"
 
 TEST_CASE("sp/SourceProcessor: Parent") {
-    SECTION("Basic Parent") {
-        std::cout << "kill me" << std::endl;
         std::shared_ptr<PKBManager> pkbManager = std::make_shared<PKBManager>();
         std::string filename = "sample.txt";
         std::string sampleProgram = "procedure proc1 {"
@@ -21,16 +19,16 @@ TEST_CASE("sp/SourceProcessor: Parent") {
                                     "   print z;"   // statement 4
                                     "   y = 6 * z;" // statement 5
                                     "   while (z > 1) {"    // statement 6
-                                    "       z = z + 1; }}"; // statement 7
+                                    "       z = z + 1;"
+                                    "   }" // statement 7
                                     "   if (z > 1) then {"  // statement 8
                                     "       z = z + 1; "    // statement 9
                                     "   } else {"
                                     "       z = z + 1;"     // statement 10
                                     "       x = 4; "        // statement 11
                                     "       while (z > 1) {"    // statement 12
-                                    "           z = z + 1; }"   // statement 13
-                                    "   }"
-                                    "   print z; }";        // statement 14
+                                    "           z = z + 1; }}}";   // statement 13
+                                    
         std::ofstream file;
         file.open(filename);
         file << sampleProgram;
@@ -41,10 +39,13 @@ TEST_CASE("sp/SourceProcessor: Parent") {
         sp.extractAndPopulate();
         std::shared_ptr<PKBReaderManager> pkbReaderManager = pkbManager->getPKBReaderManager();
         std::shared_ptr<ParentReader> parentReader = pkbReaderManager->getParentReader();
+        parentReader->getAllParents();
+        parentReader->getChild(8);
+        parentReader->getParent(12);
+        parentReader->getAllChildren();
         REQUIRE(parentReader->getAllParents() == std::unordered_set<int>({6, 8, 12}));
         REQUIRE(parentReader->getChild(8) == std::unordered_set<int>({ 9, 10, 11, 12 }));
         REQUIRE(parentReader->getParent(12) == std::unordered_set<int>({ 8 }));
         REQUIRE(parentReader->getAllChildren() == std::unordered_set<int>({ 7, 9, 10, 11, 12, 13 }));
         std::filesystem::remove(filename);
-    }
 }
