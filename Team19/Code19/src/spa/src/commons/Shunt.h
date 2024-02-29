86cd644d3a86cc09326894c4440f45f80a23684b
jianoway <77208762+jianoway@users.noreply.github.com>
2024-03-01 00:05:15 +0800

    fix: Shunting yard algo quotes (#254)
    * fix: Shunting yard algo quotes
    
    * fix: Remove use of magic number and abstract method
    
    * fix: Long method 
    
    * fix: Add Shunt
    
    * fix: Small error

---
+++ Team19/Code19/src/spa/src/commons/Shunt.h
@@ -0,0 +1,23 @@
+#pragma
+
+#include "string"
+#include "stack"
+
+class Shunt {
+private:
+    std::string postfixExpression;
+    std::stack<char> stack;
+    bool prevOperand;
+public:
+    Shunt();
+    bool getPrevOperand() const;
+    std::string getPostfixExpression();
+    std::stack<char> getStack();
+    void setPrevOperand(bool prevOperand);
+    void addStringToPostfixExpression(std::string c);
+    void addCharToPostfixExpression(char c);
+    void addToStack(char c);
+    char popFromStack();
+};
+
+
--- Team19/Code19/src/spa/src/commons/ShuntingYard.h
+++ Team19/Code19/src/spa/src/commons/ShuntingYard.h
@@ -1,18 +1,34 @@
 #pragma once
 
 #include <string>
+#include "stack"
+#include "Shunt.h"
 
 // ai-gen start(copilot, 0, e)
 // prompt: used copilot
+
+enum class Precedence {
+    INVALID = -1,
+    LOW = 1,
+    HIGH = 2,
+};
+
 class ShuntingYard {
 public:
   static std::string convertToPostfix(const std::string& infixExpression);
 private:
-  static int precedence(char c);
-  static bool isOperator(char c);
-  static bool isOperand(char c);
-  static bool isLeftParenthesis(char c);
-  static bool isRightParenthesis(char c);
-  static std::string infixToPostfix(const std::string& infixExpression);
+    static std::string makeQuoted(char c);
+    static Precedence getPrecedence(char c);
+    static bool isOperator(char c);
+    static bool isOperand(char c);
+    static bool isLeftParenthesis(char c);
+    static bool isRightParenthesis(char c);
+    static std::string infixToPostfix(const std::string& infixExpression);
+    static void handleOperand(Shunt& shunt, char c);
+    static void handleOperator(Shunt& shunt, char c);
+    static void handleLeftParenthesis(Shunt& shunt, char c);
+    static void handleRightParenthesis(Shunt& shunt);
+    static void cleanUpStack(Shunt& shunt);
+    static void handleNextCharacter(Shunt& shunt, char c);
 };
 // ai-gen end
