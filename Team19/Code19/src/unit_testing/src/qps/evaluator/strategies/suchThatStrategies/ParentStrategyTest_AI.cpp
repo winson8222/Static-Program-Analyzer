249cebabccf1cc55084e7a752e28b5cae453c80d
shurvirarora <shurvirarora@gmail.com>
2024-03-07 18:35:51 +0800

    test: Update ParentStrategyTest

--- Team19/Code19/src/unit_testing/src/qps/evaluator/strategies/suchThatStrategies/ParentStrategyTest_AI.cpp
+++ Team19/Code19/src/unit_testing/src/qps/evaluator/strategies/suchThatStrategies/ParentStrategyTest_AI.cpp
@@ -20,124 +20,164 @@ ParsingResult createParsingResultForParent(int parentStmt, int childStmt, bool i
     return parsingResult;
 }
 
-//TEST_CASE("src/qps/evaluator/suchThatStrategies/ParentStrategy/1") {
-//    auto pkb = std::make_shared<PKB>();
-//    auto parentStore = pkb->getParentStore();
-//    parentStore->addRelationship(1, 2);
-//    parentStore->addRelationship(2, 3);
-//    parentStore->addRelationship(3, 4); // Added for Parent* (transitive) testing
-//    parentStore->addRelationship(1, 3);
-//    parentStore->addRelationship(1, 4);
-//
-//    auto pkbReaderManager = std::make_shared<PKBReaderManager>(pkb);
-//    ParentStrategy parentStrategy;
-//
-//    SECTION("Direct Parent(1, 2) is true") {
-//        auto parsingResult = createParsingResultForParent(1, 2, false); // false for non-transitive
-//        auto resultTable = parentStrategy.evaluateQuery(*pkbReaderManager, parsingResult, parsingResult.getSuchThatClauses()[0]);
-//        REQUIRE(resultTable->isTableTrue()); // Expecting a non-empty result
-//    }
-//
-//    SECTION("Direct Parent(2, 3) is true") {
-//        auto parsingResult = createParsingResultForParent(2, 3, false); // false for non-transitive
-//        auto resultTable = parentStrategy.evaluateQuery(*pkbReaderManager, parsingResult, parsingResult.getSuchThatClauses()[0]);
-//        REQUIRE(resultTable->isTableTrue()); // Expecting a non-empty result
-//    }
-//
-//    SECTION("Transitive Parent*(1, 3) is true") {
-//        auto parsingResult = createParsingResultForParent(1, 3, true); // true for transitive
-//        auto resultTable = parentStrategy.evaluateQuery(*pkbReaderManager, parsingResult, parsingResult.getSuchThatClauses()[0]);
-//        REQUIRE(resultTable->isTableTrue()); // Expecting a non-empty result
-//    }
-//
-//    SECTION("Transitive Parent*(1, 4) is true") {
-//        auto parsingResult = createParsingResultForParent(1, 4, true); // true for transitive
-//        auto resultTable = parentStrategy.evaluateQuery(*pkbReaderManager, parsingResult, parsingResult.getSuchThatClauses()[0]);
-//        REQUIRE(resultTable->isTableTrue()); // Expecting a non-empty result
-//    }
-//
-//    SECTION("Transitive Parent*(1, 5) is false") {
-//        auto parsingResult = createParsingResultForParent(1, 5, true); // true for transitive
-//        auto resultTable = parentStrategy.evaluateQuery(*pkbReaderManager, parsingResult, parsingResult.getSuchThatClauses()[0]);
-//        REQUIRE_FALSE(resultTable->isTableTrue()); // Expecting an empty result
-//    }
-//}
-
-TEST_CASE("src/qps/evaluator/suchThatStrategies/ParentStrategy/2") {
+TEST_CASE("src/qps/evaluator/suchThatStrategies/ParentStrategy/") {
 std::shared_ptr<PKBManager> pkbManager = std::make_shared<PKBManager>();
 std::shared_ptr<PKBReaderManager> pkbReaderManager = pkbManager->getPKBReaderManager();
 std::shared_ptr<PKBWriterManager> pkbWriterManager = pkbManager->getPKBWriterManager();
-
 std::shared_ptr<StatementWriter> statementWriter = pkbWriterManager->getStatementWriter();
 std::shared_ptr<ParentWriter> parentWriter = pkbWriterManager->getParentWriter();
-statementWriter->insertStatement(1);
-statementWriter->insertStatement(2);
-statementWriter->insertStatement(3);
-parentWriter->addParent(1, 2);
-
-
-
-std::vector<Token> tokens = {
-        Token(TokenType::DesignEntity, "stmt"),
-        Token(TokenType::IDENT, "s"),
-        Token(TokenType::Semicolon, ";"),
-        Token(TokenType::SelectKeyword, "Select"),
-        Token(TokenType::IDENT, "s"),
-        Token(TokenType::SuchKeyword, "such"),
-        Token(TokenType::ThatKeyword, "that"),
-        Token(TokenType::Parent, "Parent"),
-        Token(TokenType::Lparenthesis, "("),
-        Token(TokenType::IDENT, "s"),
-        Token(TokenType::Comma, ","),
-        Token(TokenType::INTEGER, "2"),
-        Token(TokenType::Rparenthesis, ")")
-
-};
-
-QueryParser parser(tokens);
-auto parsingResult = parser.parse();
-QueryEvaluator evaluator(pkbReaderManager, parsingResult);
-std::unordered_set<string> res = evaluator.evaluateQuery();
-REQUIRE(res == std::unordered_set<string>{ "1" });
-
-}
-
-TEST_CASE("src/qps/evaluator/suchThatStrategies/ParentStrategy/3") {
-    std::shared_ptr<PKBManager> pkbManager = std::make_shared<PKBManager>();
-    std::shared_ptr<PKBReaderManager> pkbReaderManager = pkbManager->getPKBReaderManager();
-    std::shared_ptr<PKBWriterManager> pkbWriterManager = pkbManager->getPKBWriterManager();
-
-    std::shared_ptr<StatementWriter> statementWriter = pkbWriterManager->getStatementWriter();
-    std::shared_ptr<ParentWriter> parentWriter = pkbWriterManager->getParentWriter();
     statementWriter->insertStatement(1);
     statementWriter->insertStatement(2);
-    statementWriter->insertStatement(3);
+    parentWriter->addParent(1, 2);
     parentWriter->addParent(2, 3);
+    parentWriter->addParent(1, 3);
+
+    ParentStrategy parentStrategy;
 
 
+    SECTION("Verifying Direct Parent Relation: Parent of Statement 2") {
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "stmt"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::SuchKeyword, "such"),
+                Token(TokenType::ThatKeyword, "that"),
+                Token(TokenType::Parent, "Parent"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::INTEGER, "2"),
+                Token(TokenType::Rparenthesis, ")")
+        };
 
-    std::vector<Token> tokens = {
-            Token(TokenType::DesignEntity, "stmt"),
-            Token(TokenType::IDENT, "s"),
-            Token(TokenType::Semicolon, ";"),
-            Token(TokenType::SelectKeyword, "Select"),
-            Token(TokenType::IDENT, "s"),
-            Token(TokenType::SuchKeyword, "such"),
-            Token(TokenType::ThatKeyword, "that"),
-            Token(TokenType::Parent, "Parent"),
-            Token(TokenType::Lparenthesis, "("),
-            Token(TokenType::INTEGER, "2"),
-            Token(TokenType::Comma, ","),
-            Token(TokenType::IDENT, "s"),
-            Token(TokenType::Rparenthesis, ")")
+        QueryParser parser(tokens);
+        auto parsingResult = parser.parse();
+        QueryEvaluator evaluator(pkbReaderManager, parsingResult);
+        std::unordered_set<string> res = evaluator.evaluateQuery();
+        REQUIRE(res == std::unordered_set<string>{ "1" });
+    }
 
-    };
+    SECTION("Verifying Direct Parent Relation: Children of Statement 2") {
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "stmt"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::SuchKeyword, "such"),
+                Token(TokenType::ThatKeyword, "that"),
+                Token(TokenType::Parent, "Parent"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::INTEGER, "2"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Rparenthesis, ")")
 
-    QueryParser parser(tokens);
-    auto parsingResult = parser.parse();
-    QueryEvaluator evaluator(pkbReaderManager, parsingResult);
-    std::unordered_set<string> res = evaluator.evaluateQuery();
-    REQUIRE(res == std::unordered_set<string>{ "3" });
+        };
+        QueryParser parser(tokens);
+        auto parsingResult = parser.parse();
+        QueryEvaluator evaluator(pkbReaderManager, parsingResult);
+        std::unordered_set<string> res = evaluator.evaluateQuery();
+        REQUIRE(res == std::unordered_set<string>{ "3" });
+    }
 
+
+    SECTION("Verifying Transitive Parent Relation: Parent* of Statement 3") {
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "stmt"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::SuchKeyword, "such"),
+                Token(TokenType::ThatKeyword, "that"),
+                Token(TokenType::ParentT, "Parent*"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::INTEGER, "1"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::INTEGER, "3"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+
+        QueryParser parser(tokens);
+        auto parsingResult = parser.parse();
+        QueryEvaluator evaluator(pkbReaderManager, parsingResult);
+        std::unordered_set<std::string> res = evaluator.evaluateQuery();
+        REQUIRE(res == std::unordered_set<std::string>{"1", "2"});
+    }
+
+    SECTION("Verifying Parent Relation for Same Statement") {
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "stmt"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::SuchKeyword, "such"),
+                Token(TokenType::ThatKeyword, "that"),
+                Token(TokenType::Parent, "Parent"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::INTEGER, "1"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::INTEGER, "1"),  // Same statement as parent and child
+                Token(TokenType::Rparenthesis, ")")
+        };
+
+        QueryParser parser(tokens);
+        auto parsingResult = parser.parse();
+        QueryEvaluator evaluator(pkbReaderManager, parsingResult);
+        std::unordered_set<std::string> res = evaluator.evaluateQuery();
+        REQUIRE(res.empty());
+    }
+
+    SECTION("Verifying No Parent Relation") {
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "stmt"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::SuchKeyword, "such"),
+                Token(TokenType::ThatKeyword, "that"),
+                Token(TokenType::Parent, "Parent"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::INTEGER, "1"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::INTEGER, "4"),  // Statement 4 has no parent
+                Token(TokenType::Rparenthesis, ")")
+        };
+
+        QueryParser parser(tokens);
+        auto parsingResult = parser.parse();
+        QueryEvaluator evaluator(pkbReaderManager, parsingResult);
+        std::unordered_set<std::string> res = evaluator.evaluateQuery();
+        REQUIRE(res.empty());
+    }
+
+    SECTION("Verifying Parent Relation with Invalid Statement Numbers") {
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "stmt"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "s"),
+                Token(TokenType::SuchKeyword, "such"),
+                Token(TokenType::ThatKeyword, "that"),
+                Token(TokenType::Parent, "Parent"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::INTEGER, "10"),  // Invalid statement number
+                Token(TokenType::Comma, ","),
+                Token(TokenType::INTEGER, "2"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+
+        QueryParser parser(tokens);
+        auto parsingResult = parser.parse();
+        QueryEvaluator evaluator(pkbReaderManager, parsingResult);
+        std::unordered_set<std::string> res = evaluator.evaluateQuery();
+        REQUIRE(res.empty());
+    }
 }
 
