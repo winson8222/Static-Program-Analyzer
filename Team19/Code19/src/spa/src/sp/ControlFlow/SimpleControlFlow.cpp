f8d56028d04fe9510b01a85a79e5ca90deb02ebf
GWphua <phuaguanwei99@gmail.com>
2024-03-12 01:55:27 +0800

    Link CFGNode and ControlFlow classes

--- Team19/Code19/src/spa/src/sp/ControlFlow/SimpleControlFlow.cpp
+++ Team19/Code19/src/spa/src/sp/ControlFlow/SimpleControlFlow.cpp
@@ -1,6 +1,6 @@
 #include "SimpleControlFlow.h"
 
-SimpleControlFlow::SimpleControlFlow(std::shared_ptr<ASTNode> root): root(root);
+SimpleControlFlow::SimpleControlFlow(std::shared_ptr<ASTNode> root) : root(root) {}
 
 std::shared_ptr<CFGNode> SimpleControlFlow::createControlFlowGraph() {
 	return std::make_shared<CFGNode>();
--- Team19/Code19/src/spa/src/sp/ControlFlow/SimpleControlFlow.h
+++ Team19/Code19/src/spa/src/sp/ControlFlow/SimpleControlFlow.h
@@ -1,21 +1,21 @@
 #pragma once
 
+#include "sp/ControlFlow/CFGNode.h"
 #include "sp/AST/ASTNode.h"
-#include "sp/AST/ASTUtility.h"
 #include <unordered_map>
 
 /*
-* This class is used to link all calls to procedures to the procedure node.
+* @brief This class is used to generate a CFG Representation of the source SIMPLE program.
 */
 class SimpleControlFlow {
 public:
 	/*
-	* Constructor for Control Flow Graph generator that takes in the root of the AST.
+	* @brief Constructor for Control Flow Graph generator that takes in the root of the AST.
 	*/
 	SimpleControlFlow(std::shared_ptr<ASTNode> root);
 
 	/*
-	* Links all calls to procedures to the procedure node.
+	* @brief Creates the control flow graph based on the AST representation of the SIMPLE program.
 	*/
 	std::shared_ptr<CFGNode> createControlFlowGraph();
 private:
