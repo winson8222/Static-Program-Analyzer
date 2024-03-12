9529150d69858cf992d1ecd122ff189163973463
GWphua <phuaguanwei99@gmail.com>
2024-03-12 17:56:23 +0800

    Fix Mac CICD issues

--- Team19/Code19/src/unit_testing/src/sp/ControlFlowTest/CFGNodeTest_AI.cpp
+++ Team19/Code19/src/unit_testing/src/sp/ControlFlowTest/CFGNodeTest_AI.cpp
@@ -2,7 +2,7 @@
 #include "sp/ControlFlow/CFGNode.h"
 
 TEST_CASE("CFGNode default constructor") {
-    auto& node = std::make_shared<CFGNode>();
+    auto node = std::make_shared<CFGNode>();
     REQUIRE(node->getLineNumber() == -1);
     REQUIRE(node->getChildren().empty() == true);
     REQUIRE(node->isEndOfProcedure() == true);
@@ -10,14 +10,14 @@ TEST_CASE("CFGNode default constructor") {
 
 TEST_CASE("CFGNode constructor with line number") {
     int line_number = 10;
-    auto& node = std::make_shared<CFGNode>(line_number);
+    auto node = std::make_shared<CFGNode>(line_number);
     REQUIRE(node->getLineNumber() == line_number);
     REQUIRE(node->getChildren().empty() == true);
     REQUIRE(node->isEndOfProcedure() == false);
 }
 
 TEST_CASE("CFGNode end of procedure node") {
-    auto& node = CFGNode::getDummyNode();
+    auto node = CFGNode::getDummyNode();
     REQUIRE(node->getLineNumber() == CFGNode::PROCEDURE_END_LINE_NUMBER);
     REQUIRE(node->getChildren().empty() == true);
     REQUIRE(node->isEndOfProcedure() == true);
@@ -29,4 +29,16 @@ TEST_CASE("Child addition to CFGNode", "[CFGNode]") {
     parent->addChild(child);
     REQUIRE(parent->getChildren().size() == 1);
     REQUIRE(parent->getChildren()[0] == child);
+}
+
+TEST_CASE("While loop addition to CFGNode", "[CFGNode]") {
+    auto parent = std::make_shared<CFGNode>();
+    auto child = std::make_shared<CFGNode>();
+    parent->addChild(child);
+    child->addChild(parent);
+    REQUIRE(parent->getChildren().size() == 1);
+    REQUIRE(parent->getChildren()[0] == child);
+
+    REQUIRE(child->getChildren().size() == 1);
+    REQUIRE(child->getChildren()[0] == parent);
 }
