8d5bb2a794799314690193b0209f18aaee846ac7
hmanhduc2k <hmanhduc2k@gmail.com>
2024-03-17 16:27:04 +0800

    Implement CallProc Extractors on DE

--- Team19/Code19/src/spa/src/sp/DesignExtractor/Extractor/RelationshipExtractor/CallsPExtractor.h
+++ Team19/Code19/src/spa/src/sp/DesignExtractor/Extractor/RelationshipExtractor/CallsPExtractor.h
@@ -35,3 +35,21 @@ public:
 private:
 	std::shared_ptr<CallsTWriter> callWriter;
 };
+
+/*
+* Extracts the relations of procedure calling any procedure name
+* from the AST and writes the information to the PKB.
+* This class implements the IExtractor interface. The methods are documented
+* from the base class at IExtractor.h
+*/
+class CallProcNameExtractor : public IRelationshipExtractor {
+public:
+	CallProcNameExtractor(std::shared_ptr<ASTNode> ast1,
+		std::shared_ptr<ASTNode> ast2,
+		std::shared_ptr<CallProcNameWriter> callWriter);
+
+	void extract() override;
+
+private:
+	std::shared_ptr<CallProcNameWriter> callWriter;
+};
