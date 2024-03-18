b6cb4c11c6564329a603065c5e90cc14b2bef1bb
winson8222 <winson8222@hotmail.com>
2024-03-18 19:28:45 +0800

    Parser support if while pattern and added tests

--- Team19/Code19/src/unit_testing/src/qps/parser/QueryParserTest.cpp
+++ Team19/Code19/src/unit_testing/src/qps/parser/QueryParserTest.cpp
@@ -893,3 +893,394 @@ TEST_CASE("src/qps/parser/QueryParser/4") {
         REQUIRE(parsingResult.isQueryValid() == true);
     }
 }
+
+TEST_CASE("src/qps/parser/QueryParser/5") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses Pattern with while syn") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "while"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::QuoutIDENT, "\"x\""),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "w");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "w");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "while");
+        REQUIRE(patternClause.getFirstParam().getValue() == "\"x\"");
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue().empty());
+    }
+}
+
+
+TEST_CASE("src/qps/parser/QueryParser/6") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser throw semantic error when parsing Pattern with QuoutIDENT and wildcard") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "while"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::QuoutIDENT, "\"x\""),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::QuoutIDENT, "\"x\""),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "SemanticError");
+        REQUIRE(parsingResult.isQueryValid() == false);
+
+    }
+}
+
+TEST_CASE("src/qps/parser/QueryParser/7") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses while Pattern with while syn with wildcards") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "while"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "w");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "w");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "while");
+        REQUIRE(patternClause.getFirstParam().getValue() == "_");
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue().empty());
+
+    }
+}
+
+
+TEST_CASE("src/qps/parser/QueryParser/8") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses while Pattern with while syn") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "while"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "w");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "w");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "while");
+        REQUIRE(patternClause.getFirstParam().getValue() == "_");
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue().empty());
+
+    }
+}
+
+
+TEST_CASE("src/qps/parser/QueryParser/9") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses while Pattern with var syn and wildcards") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "while"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::DesignEntity, "variable"),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "v");
+        REQUIRE(parsingResult.getRequiredSynonymType(parsingResult.getRequiredSynonyms()[0]) == "variable");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "w");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "while");
+        REQUIRE(patternClause.getFirstParam().getValue() == "v");
+        REQUIRE(patternClause.getFirstParam().getType() == TokenType::IDENT);
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue().empty());
+
+    }
+}
+
+TEST_CASE("src/qps/parser/QueryParser/10") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses while Pattern with var syn and wildcards") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "while"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::DesignEntity, "variable"),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "w"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "v");
+        REQUIRE(parsingResult.getRequiredSynonymType(parsingResult.getRequiredSynonyms()[0]) == "variable");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "w");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "while");
+        REQUIRE(patternClause.getFirstParam().getValue() == "v");
+        REQUIRE(patternClause.getFirstParam().getType() == TokenType::IDENT);
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue().empty());
+
+    }
+}
+
+TEST_CASE("src/qps/parser/QueryParser/11") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses Pattern with if syn") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "if"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::QuoutIDENT, "\"x\""),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "ifs");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "ifs");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "if");
+        REQUIRE(patternClause.getFirstParam().getValue() == "\"x\"");
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue() == "_");
+    }
+}
+
+
+TEST_CASE("src/qps/parser/QueryParser/12") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser throw semantic error when parsing if Pattern with QuoutIDENT without wildcard") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "if"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::QuoutIDENT, "\"x\""),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::QuoutIDENT, "\"x\""),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::QuoutIDENT, "\"x\""),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "SemanticError");
+        REQUIRE(parsingResult.isQueryValid() == false);
+
+    }
+}
+
+TEST_CASE("src/qps/parser/QueryParser/13") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses while Pattern with if syn with wildcards") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "if"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "ifs");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "ifs");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "if");
+        REQUIRE(patternClause.getFirstParam().getValue() == "_");
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue() == "_");
+
+    }
+}
+
+
+
+TEST_CASE("src/qps/parser/QueryParser/14") {
+    //Select r such that Uses(r, 1)
+    SECTION("QueryParser correctly parses if Pattern with var syn and wildcards") {
+        // Manually create the vector of tokens for the query
+        std::vector<Token> tokens = {
+                Token(TokenType::DesignEntity, "if"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::DesignEntity, "variable"),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::Semicolon, ";"),
+                Token(TokenType::SelectKeyword, "Select"),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::PatternKeyword, "pattern"),
+                Token(TokenType::IDENT, "ifs"),
+                Token(TokenType::Lparenthesis, "("),
+                Token(TokenType::IDENT, "v"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Comma, ","),
+                Token(TokenType::Wildcard, "_"),
+                Token(TokenType::Rparenthesis, ")")
+        };
+        QueryParser queryParser(tokens);
+        // Parse the query
+        ParsingResult parsingResult = queryParser.parse();
+
+
+        // Verify that the parsing result indicates a valid query with no errors
+        REQUIRE(parsingResult.getErrorMessage() == "");
+        REQUIRE(parsingResult.isQueryValid() == true);
+        REQUIRE(parsingResult.getRequiredSynonyms()[0] == "v");
+        REQUIRE(parsingResult.getRequiredSynonymType(parsingResult.getRequiredSynonyms()[0]) == "variable");
+
+        PatternClause patternClause = parsingResult.getPatternClauses()[0];
+        REQUIRE(patternClause.getRelationship().getValue() == "ifs");
+        REQUIRE(parsingResult.getPatternClauseType(patternClause) == "if");
+        REQUIRE(patternClause.getFirstParam().getValue() == "v");
+        REQUIRE(patternClause.getFirstParam().getType() == TokenType::IDENT);
+        REQUIRE(patternClause.getSecondParam().getValue() == "_");
+        REQUIRE(patternClause.getThirdParam().getValue() == "_");
+
+    }
+}
