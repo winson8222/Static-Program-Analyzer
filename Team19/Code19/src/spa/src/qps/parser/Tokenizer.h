9cf95fa63a70863a5d88738226383f7204027e87
shurvirarora <shurvirarora@gmail.com>
2024-03-24 23:19:59 +0800

    chore: update comment typo

--- Team19/Code19/src/spa/src/qps/parser/Tokenizer.h
+++ Team19/Code19/src/spa/src/qps/parser/Tokenizer.h
@@ -39,8 +39,8 @@ private:
     vector<Token> tokens;
     bool isBooleanDeclared = false; //special case for boolean because it is treated as a synonym after selection if it was declared
 
-    // A private method to determine the type of a token given its string representation.
-    // Returns the TokenType of the token which is usefull in the QueryParser.
+    // A private method to determine the type of token given its string representation.
+    // Returns the TokenType of the token which is useful in the QueryParser.
     TokenType determineTokenType(const string& tokenStr);
     // helper fn for determineTokenType
     TokenType determineClauseKeywordToken(const string& tokenStr);
