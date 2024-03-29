529627917dbfbb16102c326d4df6b995521240dd
winson8222 <winson8222@hotmail.com>
2024-03-29 16:48:21 +0800

    "Fixed merge conflict"

--- Team19/Code19/src/unit_testing/src/qps/evaluator/strategies/WithStrategy/WithStrategyTest.cpp
+++ Team19/Code19/src/unit_testing/src/qps/evaluator/strategies/WithStrategy/WithStrategyTest.cpp
@@ -309,12 +309,12 @@ TEST_CASE("Multiple Clauses with with") {
     shared_ptr<VariableWriter> variableWriter = pkbWriterManager->getVariableWriter();
     shared_ptr<PrintVarNameWriter> printVarNameWriter = pkbWriterManager->getPrintVarNameWriter();
     shared_ptr<PrintWriter> printWriter = pkbWriterManager->getPrintWriter();
-    statementWriter->insertStatement(1);
-    statementWriter->insertStatement(2);
     statementWriter->insertStatement(3);
-    statementWriter->insertStatement(4);
-    statementWriter->insertStatement(5);
-    statementWriter->insertStatement(6);
+    statementWriter->insertStatement(22);
+    statementWriter->insertStatement(32);
+    statementWriter->insertStatement(7);
+    statementWriter->insertStatement(18);
+    statementWriter->insertStatement(7);
     variableWriter->insertVariable("n");
     variableWriter->insertVariable("x");
     variableWriter->insertVariable("y");
@@ -333,8 +333,6 @@ TEST_CASE("Multiple Clauses with with") {
     printVarNameWriter->addPrintVarName(7, "total");
     printWriter->insertPrint(18);
     printVarNameWriter->addPrintVarName(18, "middle");
-//    printWriter->insertPrint(7);
-//    printVarNameWriter->addPrintVarName(7, "end");
 //    printWriter->insertPrint(8);
 //    printVarNameWriter->addPrintVarName(8, "variable");
 
