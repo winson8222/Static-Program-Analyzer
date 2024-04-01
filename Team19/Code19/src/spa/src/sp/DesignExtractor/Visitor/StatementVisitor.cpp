ba290e20b3594a467f6be49fa9156e61adf8b8da
hmanhduc2k <hmanhduc2k@gmail.com>
2024-04-02 03:18:33 +0800

    Add system test cases for Parent* and optimize Parent DE

--- Team19/Code19/src/spa/src/sp/DesignExtractor/Visitor/StatementVisitor.cpp
+++ Team19/Code19/src/spa/src/sp/DesignExtractor/Visitor/StatementVisitor.cpp
@@ -3,21 +3,17 @@
 void StatementVisitor::setParents(listnode contexts, std::shared_ptr<ASTNode> root, std::shared_ptr<PKBWriterManager> pkbWriterManager) {
     int size = contexts.size();
     int curr = size - 1;
+
+    if (curr >= 0 && !contexts[curr]->equalType(ASTNodeType::PROCEDURE) && !contexts[curr]->equalType(ASTNodeType::CALL)) {
+        ParentExtractor parentExtractor(contexts[curr], root, pkbWriterManager->getParentWriter());
+        parentExtractor.extract();
+    }
+
     std::shared_ptr<ASTNode> startPoint = contexts[curr];
     while (curr >= 0 && (!startPoint->equalType(ASTNodeType::PROCEDURE)
         && !startPoint->equalType(ASTNodeType::CALL))) {
-        startPoint = contexts[--curr];
-    }
-
-    for (int i = curr; i < size; i++) {
-        std::shared_ptr<ASTNode> context = contexts[i];
-        if (context->equalType(ASTNodeType::PROCEDURE)) continue;
-        ParentTExtractor parentExtractor(context, root, pkbWriterManager->getParentTWriter());
+        ParentTExtractor parentExtractor(startPoint, root, pkbWriterManager->getParentTWriter());
         parentExtractor.extract();
-    }
-
-    if (size > 0 && !contexts[size - 1]->equalType(ASTNodeType::PROCEDURE) && !contexts[size - 1]->equalType(ASTNodeType::CALL)) {
-        ParentExtractor parentExtractor(contexts[size - 1], root, pkbWriterManager->getParentWriter());
-        parentExtractor.extract();
+        startPoint = contexts[curr--];
     }
 }
